231aff203f9ab33533ba60441dc652e0a5e9365b
awasunyin <awasunyin@gmail.com>
2018-02-17 08:04:36 -0700

    added erc721 standard and updated ramenlab

--- ramenlab.sol
+++ ramenlab.sol
@@ -12,76 +12,16 @@ contract RamenLab is RamenHelper {
 
     // NOTE: Probably doesn't make sense to store on the blockchain
     // the available ramen with all the combinations
-
-    struct Noodles {
-        string firmness;
-    }
-
-    Noodles[] firmnessTypes;
-
-    // hard coded some properties
-    function _addFirmnessTypes(string _name) private returns(Noodles[]) {
-        firmnessTypes[0] = "soft";
-        firmnessTypes[1] = "medium";
-        firmnessTypes[2] = "firm";
-        return firmnessTypes;
-    }
-
-    struct Both {
-        string broth;
-    }
-
-    Broth[] availableBroths;
-
-    function _addBroth(string _broth) private returns(Broth[]) {
-        availableBroths[0] = "tonkotsu";
-        availableBroths[1] = "shio";
-        availableBroths[2] = "miso";
-    }
-
-    //TODO: continue editing this
-
-    struct Flavouring {
-        string flavour;
-    }
-
-    Flavouring[] availableFlavourings;
-
-    function _addFlavouring(string _flavour) private returns(Broth[]) {
-        availableFlavourings[0] = "tonkotsu";
-        availableFlavourings[1] = "shio";
-        availableFlavourings[2] = "miso";
-    }
-
-    struct Topping {
+    struct Ramen {
+        string name;
         string topping;
     }
 
-    Topping[] availableToppings;
+    // plural of ramen is ramen
+    Ramen[] ramen;
 
-    function _addBroth(string _broth) private returns(Broth[]) {
-        availableToppings[0] = "tonkotsu";
-        availableToppings[1] = "shio";
-        availableToppings[2] = "miso";
-    }
+    function _addRamen(Ramen storage _ramen) internal {
+  // do stuff with _zombie
 
-    function getRamen(uint _id) external view returns (
-        string ramenName,
-        string ramenNoodles,
-        string ramenBroth,
-        string ramenFlavouring,
-        string ramenTopping1,
-        string ramenTopping2
-
-    ) { 
-        Ramen storage ramen = ramen[_id];
-        
-        ramenName = 
-        ramenNoodles =
-        ramenBroth =
-        ramenFlavouring =
-        ramenTopping1 =
-        ramenTopping2 =
-
-    }
+    }   
 }
